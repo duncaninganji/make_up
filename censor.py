c7fbebd936ffac72de17014f224688ca43a300e5
duncaninganji <duncan.inganji@berkeley.edu>
2015-12-14 15:43:41 -0800

    Fifth Commit

--- censor.py
+++ censor.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
-original = []
+Texts = []
 bad_words = ["is", "are", "have", "no", "many", "esteem"]
 
 
@@ -16,23 +16,29 @@ def start():
 def revert():
 	Line1 = "We are here for all your censoring needs. "
 	Line2 = "Here is your original text."
-	Line3 = original[0]
+	Line3 = Texts[-1][0]
 	return render_template('index.html', Line1=Line1, Line2=Line2, Line3=Line3)
 
 @app.route('/censored', methods = ['POST'])
 def censor():
 	Line1 = "Thank you for trusting us with your censoring needs. "
 	Line2 = "Below find your censored text."
-	text = request.form['my_text']
-	original.append(text)
+	text, pair=request.form['my_text'], []
+	pair.append(text)
 	words = to_lst(text)
 	for word in words:
 		if word in bad_words:
 			words[words.index(word)] = "*CENSORED*"
 	text = to_str(words)
+	pair.append(text)
+	Texts.append(pair)
 	Line3 = text
 	return render_template('index.html', Line1=Line1, Line2=Line2, Line3=Line3)
-	
+
+@app.route('/history')
+def history():
+	return render_template('censored.html', Texts=Texts)	
+
 def to_str(lst):
 	my_str = ""
 	for word in lst:
